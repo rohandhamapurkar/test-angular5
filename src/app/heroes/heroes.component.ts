04bad24b72f0c50789c2c346366f71678a0f24e0
786kaizersoze <dhamapurkar54@gmail.com>
2018-01-21 22:32:48 +0530

    test Angular 5 commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes : Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  
+  constructor(private heroService: HeroService) { 
+
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Jack Nicholson' },
+    { id: 12, name: 'Al Pacino' },
+    { id: 13, name: 'Robert De Niro' },
+    { id: 14, name: 'Robert Downey Jr.' },
+    { id: 15, name: 'Will Smith' },
+    { id: 16, name: 'Morgan Freeman' },
+    { id: 17, name: 'Samuel L. Jackson' },
+    { id: 18, name: 'Brad Pitt' },
+    { id: 19, name: 'Quentin Tarantino' },
+    { id: 20, name: 'Chistoph Waltz' },
+    { id: 21, name: 'Liam Neeson' }
+];
